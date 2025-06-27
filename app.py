7b70ed1d6f36252d0b5145b55358de7e47f3f6b9
Manish Reddy <159930828+Manish1244@users.noreply.github.com>
2025-06-27 20:38:25 +0530

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 import requests
+import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-# Judge0 API Endpoint
-JUDGE0_URL = "https://judge0-ce.p.rapidapi.com/submissions"
 JUDGE0_HEADERS = {
-    "X-RapidAPI-Key": "<YOUR_RAPIDAPI_KEY>",
+    "X-RapidAPI-Key": os.environ.get("RAPIDAPI_KEY"),
     "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
     "content-type": "application/json"
 }
@@ -17,20 +20,28 @@ JUDGE0_HEADERS = {
 def index():
     return render_template('index.html')
 
-@app.route('/run', methods=['POST'])
-def run_code():
-    data = request.get_json()
-    response = requests.post(JUDGE0_URL + "?base64_encoded=false&wait=true",
-                             json=data, headers=JUDGE0_HEADERS)
-    return jsonify(response.json())
-
 @socketio.on('code_change')
-def handle_code_change(data):
+def code_change(data):
     emit('code_update', data, broadcast=True, include_self=False)
 
 @socketio.on('chat_message')
 def handle_chat(msg):
     emit('chat_message', msg, broadcast=True)
 
+@app.route('/run', methods=['POST'])
+def run_code():
+    payload = {
+        "language_id": 63,
+        "source_code": request.json.get("code"),
+        "stdin": ""
+    }
+    response = requests.post(
+        "https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=true",
+        json=payload,
+        headers=JUDGE0_HEADERS
+    )
+    return jsonify(response.json())
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
